853042488126632ff09ac1dcc17cfc1a42c6ebc4
ROH <3193934857@qq.com>
2025-07-19 12:24:03 +0800

    阴阳界限一改

--- Content/Items/Cameras/YukariCamera.cs
+++ Content/Items/Cameras/YukariCamera.cs
@@ -13,6 +13,7 @@ using AyaMod.Core;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent;
 using Microsoft.Xna.Framework.Graphics.PackedVector;
+using Terraria.Audio;
 
 namespace AyaMod.Content.Items.Cameras
 {
@@ -55,7 +56,7 @@ namespace AyaMod.Content.Items.Cameras
             if (projectile.type != ProjectileType<YinYangBall>()) return;
             if (projectile.localAI[2] < 1)
             {
-                projectile.localAI[2] = 12f;
+                projectile.localAI[2] = 6f;
                 Vector2 dir = Projectile.DirectionToSafe(projectile.Center);
                 projectile.velocity = dir * 18f;
                 Projectile.localAI[2] = 1;
@@ -97,24 +98,44 @@ namespace AyaMod.Content.Items.Cameras
             Projectile.ignoreWater = true;
             Projectile.penetrate = -1;
             Projectile.SetImmune(15);
-
+            Projectile.scale = 1.5f;
             Projectile.timeLeft = 99999999;
         }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
+            var prev = Projectile.localAI[2];
+
             Projectile.localAI[1] = target.whoAmI;
             Projectile.localAI[2]--;
 
+            var othertarget = Projectile.FindCloestNPCIgnoreIndex(4000, false, !Projectile.tileCollide, (int)Projectile.localAI[1]);
+            if (othertarget == null)
+            {
+                Projectile.localAI[1] = -1;
+                Projectile.localAI[2] = 0;
+            }
+            else Projectile.localAI[0] = othertarget.whoAmI;
 
+            if (Projectile.localAI[2] == 0 && Projectile.velocity.Length() > 10)
+            {
+                Projectile.velocity = Projectile.velocity.Length(10);
+            }
 
             Vector2 totarget = Projectile.Center.DirectionToSafe(target.Center);
+            if (Projectile.localAI[2] == 0 && prev > 0) totarget = totarget.RotatedBy(MathHelper.PiOver4 * (Main.rand.NextBool()?1:-1));
             Vector2 vec = (-Projectile.velocity).Reflect(totarget.RotatedBy(MathHelper.PiOver2));
             Projectile.velocity = vec;
             //Projectile.velocity *= -1;
             //Projectile.velocity = Projectile.velocity.RotatedBy(dir * MathHelper.PiOver2);
-            if (Projectile.velocity.Length() < 6f) Projectile.velocity = Projectile.velocity.Length(12);
+            if (Projectile.velocity.Length() < 6f) Projectile.velocity = Projectile.velocity.Length(8);
 
             if (Projectile.velocity.Length() > 5f) CreateWaveDust(Projectile.Center + totarget * 22f, totarget.ToRotation());
+
+            if(prev > 0)
+            {
+                //89,88,74,73
+                SoundEngine.PlaySound(SoundID.Item70 with {  Pitch = 0.5f, Volume = 1f, MaxInstances = 20}, Projectile.Center);
+            }
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
@@ -130,16 +151,16 @@ namespace AyaMod.Content.Items.Cameras
         public void CreateWaveDust(Vector2 center, float rot)
         {
 
-            int dustamount = 72;
+            int dustamount = 48;
             for(int i = 0; i < dustamount; i++)
             {
                 int dustType = Main.rand.NextBool() ? DustID.RedTorch : DustID.WhiteTorch;
                 float dir = MathHelper.TwoPi / dustamount * i;
                 Vector2 vec = dir.ToRotationVector2();
                 float scale = Utils.Remap(MathF.Abs(vec.X), 0f, 1f, 1f, 0.5f);
-                vec.X /= 3f;
+                vec.X /= 2.5f;
                 vec = vec.RotatedBy(rot);
-                Dust d = Dust.NewDustPerfect(center, dustType, vec * 3.5f,Scale:scale * 3f);
+                Dust d = Dust.NewDustPerfect(center + vec * 10f, dustType, vec * 2.2f,Scale:scale * 5f);
                 d.noGravity = true;
             }
         }
@@ -162,23 +183,36 @@ namespace AyaMod.Content.Items.Cameras
                 if (Projectile.localAI[2] < 1)
                 {
                     Vector2 targetVel = Projectile.DirectionToSafe(camera.Center) * 20f;
-                    Projectile.velocity = Vector2.Lerp(Projectile.velocity, targetVel, 0.03f);
+                    Projectile.velocity = Vector2.Lerp(Projectile.velocity, targetVel, 0.01f);
                 }
                 else
                 {
                     //if (Projectile.velocity.Length() < 4f) Projectile.localAI[2] = 0;
                     float range = 3000;
-                    NPC target = Projectile.FindCloestNPCIgnoreIndex(range, false, !Projectile.tileCollide, (int)Projectile.localAI[1]);
+                    NPC target = Main.npc[(int)Projectile.localAI[0]];
+                    if (target == null || !target.CanBeChasedBy() || Projectile.localAI[0] == Projectile.localAI[1])
+                    {
+                        target = Projectile.FindCloestNPCIgnoreIndex(range, false, !Projectile.tileCollide, (int)Projectile.localAI[1]);
+                        if (target != null)
+                        {
+                            Projectile.localAI[0] = target.whoAmI;
+                        }
+                    }
 
                     if (target != null)
                     {
-                        float chaseFactor = Utils.Remap(Projectile.Distance(target.Center) / 600, 0f, 1f, 0.03f, 0.07f);
-                        Projectile.Chase(4000, 25, chaseFactor * 1.1f);
+                        float speed = 28f;
+                        float speedFactor = Utils.Remap(Projectile.Distance(target.Center), 500, 1000, 1.3f, 2.8f);
+                        
+                        Projectile.velocity = Projectile.DirectionToSafe(target.Center) * speed;
+                        //float chaseFactor = Utils.Remap(Projectile.Distance(target.Center) / 600, 0f, 1f, 0.03f, 0.07f);
+                        //Projectile.Chase(target, 25, chaseFactor * 1.2f);
                     }
                     else
                     {
                         Projectile.localAI[1] = -1;
                         Projectile.localAI[2] = 0;
+                        if (Projectile.velocity.Length() > 12) Projectile.velocity = Projectile.velocity.Length(12);
                     }
                 }
             }
@@ -210,8 +244,13 @@ namespace AyaMod.Content.Items.Cameras
                 float factor = 1f - (float)i / Projectile.oldPos.Length;
                 float rot = i == 0 ? Projectile.rotation : Projectile.oldRot[i];
                 Vector2 drawpos = Projectile.oldPos[i] + Projectile.Size / 2f - Main.screenPosition;
-                Color trailWhiteColor = trailBaseWhiteColor * factor * 0.6f;
-                Color trailColor = trailBaseColor * factor * 0.6f;
+                Color trailWhiteColor = trailBaseWhiteColor * factor * 0.9f;
+                Color trailColor = trailBaseColor * factor * 0.9f;
+                if (Projectile.localAI[2] < 1)
+                {
+                    trailWhiteColor *= 0.4f;
+                    trailColor *= 0.4f;
+                }
                 float scale = Projectile.scale * factor;
                 Main.spriteBatch.Draw(whitePart, drawpos, null, trailWhiteColor, rot, origin, scale, 0, 0);
                 Main.spriteBatch.Draw(colorPart, drawpos, null, trailColor, rot, origin, scale, 0, 0);
