688cb953b41d641d5c53346beb9cccb61352a0dd
ROH <3193934857@qq.com>
2025-12-25 22:11:13 +0800

    前缀合并

--- Content/Prefixes/CameraPrefixes/BasicCameraPrefixes.cs
+++ Content/Prefixes/CameraPrefixes/BasicCameraPrefixes.cs
@@ -11,6 +11,61 @@ using Terraria.ModLoader;
 
 namespace AyaMod.Content.Prefixes.CameraPrefixes
 {
+    public class Peerless() : BaseCameraPrefix(damageMult: 1.15f, focusSpeedMult: 0.9f, 5, sizeMult: 1.1f, stunMult: 1.15f, 1.75f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Fashionable() : BaseCameraPrefix(damageMult: 1.05f, focusSpeedMult: 0.9f, critBonus: 15, sizeMult: 1.1f, stunMult: 1.2f, valueMult: 1.75f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Dazzling() : BaseCameraPrefix(damageMult: 1.20f, focusSpeedMult: 1.1f, critBonus: 15, stunMult: 2f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Global() : BaseCameraPrefix(damageMult: 1.05f, focusSpeedMult: 1.05f, critBonus: 5, sizeMult: 1.3f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Realistic() : BaseCameraPrefix(critBonus: 30, sizeMult: 1.1f, stunMult: 1.1f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Ephemeral() : BaseCameraPrefix(focusSpeedMult: 0.7f, sizeMult: 1.05f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Focused() : BaseCameraPrefix(damageMult: 1.45f, focusSpeedMult: 1.1f, sizeMult: 1.15f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Precise() : BaseCameraPrefix(damageMult: 1.4f, sizeMult: 0.8f, valueMult: 1.65f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Sensitive() : BaseCameraPrefix(damageMult: 1.1f, sizeMult: 1.05f, stunMult: 1.1f, valueMult: 1.17f)
+    {
+        public override float RollChance(Item item) => 0.66f;
+    }
+    public class Wideangle() : BaseCameraPrefix(sizeMult: 1.1f, stunMult: 1.05f, valueMult: 1.06f)
+    {
+    }
+    public class Jammed() : BaseCameraPrefix(focusSpeedMult: 1.15f, stunMult: 0.9f, valueMult: 0.9f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Blurred() : BaseCameraPrefix(damageMult: 0.85f, critBonus: -10, valueMult: 0.85f)
+    {
+        public override float RollChance(Item item) => 0.9f;
+    }
+    public class Overexposed() : BaseCameraPrefix(critBonus: -10, sizeMult: 0.9f, valueMult: 0.76f)
+    {
+        public override float RollChance(Item item) => 0.66f;
+    }
+    public class Deceptive() : BaseCameraPrefix(damageMult: 0.85f, sizeMult: 0.9f, stunMult: 0.85f, valueMult: 0.7f)
+    {
+        public override float RollChance(Item item) => 0.66f;
+    }
     public abstract class BaseCameraPrefix(float damageMult = 1f, float focusSpeedMult = 1f, int critBonus = 0, 
         float sizeMult = 1f, float stunMult = 1f, float valueMult = 1f) : ModPrefix
     {
